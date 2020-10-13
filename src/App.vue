f117bc4da6500bb867f1456211bebc9c2050e5d9
shqming <1561445954@qq.com>
2020-10-13 10:56:45 +0800

    配置路由页面

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
     <!-- 路由的出口 -->
     <router-view/>
-	<van-button type="primary">主要按钮</van-button>
-	<van-button type="info">信息按钮</van-button>
-	<van-button type="default">默认按钮</van-button>
-	<van-button type="warning">警告按钮</van-button>
-	<van-button type="danger">危险按钮</van-button>
-	<!-- 手机图标 -->
-	<i class="toutiao toutiao-shouji"></i>
-	
-	<!-- 收藏图标 -->
-	<i class="toutiao toutiao-shoucang"></i>
-	<van-icon name="chat-o" badge="9" />
   </div>
 </template>
 
